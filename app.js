a05388dfa1d1fb6c7f8e0527aa5b37f78424c1be
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-01 19:27:48 +0300

    add the post and get

--- app.js
+++ app.js
@@ -9,11 +9,32 @@ mongoose.connect(`mongodb+srv://root:py6czQnOyXhFPkng@cluster0.ybpep9u.mongodb.n
 .then(()=>{
     console.log(`Connected to mongo DB`)
 })
+
+const Plants = mongoose.model('Plants', {title: String, price: Number})
+app.post('/add-plants', async (req, res) => {
+    try {
+        const { title, price } = req.body;
+        const plants = new Plants({ title, price });
+        await plants.save();
+        console.log(`Plants created`);
+        res.status(201).json(plants);
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
+})
+app.get('/plants', async (req, res)=>{
+    try {
+        const plants = await Plants.find();
+        res.json(plants)
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
+})
 app.get('/', (req, res)=>{
     res.sendFile('public', 'index.html')
 })
 app.get('/admin', (req, res)=>{
-    res.sendFile('public', 'admin', 'index.html')
+    res.sendFile(path.join(__dirname, 'public'))
 })
 app.listen(PORT, ()=>{
     console.log(`Server work on PORT: ${PORT}`)
