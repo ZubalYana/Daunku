c4402e63ea3672e884bcd8b36980898c60a3203d
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-01 18:54:40 +0300

    connect mongo DB database

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const path = require('path')
 const PORT = 3000;
 const mongoose = require('mongoose')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+mongoose.connect(`mongodb+srv://root:py6czQnOyXhFPkng@cluster0.ybpep9u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=>{
+    console.log(`Connected to mongo DB`)
+})
 app.get('/', (req, res)=>{
     res.sendFile('public', 'index.html')
 })
