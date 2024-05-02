3d40674e18cb2ae27c40c6efd094541600a8b874
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-02 23:29:55 +0300

    fix the elements position bug and general plant structure

--- public/main.js
+++ public/main.js
@@ -3,16 +3,18 @@ axios.get('http://localhost:3000/plants')
     console.log(res.data)
     for(let el of res.data){
         $('.plantsContainer').append(
-            `<div class="plant">
+            `
+            <div class="plant">
             <div class="plant_imgCon">
-            <img class="plant_img" src="./imgs/${el.image}">
-            <div class="plant_textCon">
-            <div class="plant_title">${el.title}<div>
-            <div class="plant_rating"><div>
-            <div class="plant_price">$${el.price}.00<div>
-            <div>
-            <div>
-            <div>`
+                <img class="plant_img" src="./imgs/${el.image}">
+                <div class="plant_textCon">
+                    <div class="plant_title">${el.title}</div>
+                    <div class="plant_rating"></div>
+                    <div class="plant_price">$${el.price}.00</div>
+                </div>
+            </div>
+        </div>
+            `
         )
         if(el.rating == 5){
             $('.plant_rating').append(
