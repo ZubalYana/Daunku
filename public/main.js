e6f7ad8fb28c468c99d51c9d9308ce64d8c5d290
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-02 23:28:48 +0300

    add the 5 star rating

--- public/main.js
+++ public/main.js
@@ -5,16 +5,23 @@ axios.get('http://localhost:3000/plants')
         $('.plantsContainer').append(
             `<div class="plant">
             <div class="plant_imgCon">
-            <img class="plant_img", src="./imgs/${el.image}">
+            <img class="plant_img" src="./imgs/${el.image}">
             <div class="plant_textCon">
             <div class="plant_title">${el.title}<div>
-            <div class="plant_pating">${el.rating}<div>
+            <div class="plant_rating"><div>
             <div class="plant_price">$${el.price}.00<div>
             <div>
             <div>
-
-
             <div>`
         )
+        if(el.rating == 5){
+            $('.plant_rating').append(
+                `<i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>`
+            )
+        }
     }
 })
