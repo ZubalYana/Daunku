1a5871fd6cff41683386eacd208c3ed78ce6d485
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-02 22:18:46 +0300

    fix the text elements positions

--- public/main.js
+++ public/main.js
@@ -8,8 +8,9 @@ axios.get('http://localhost:3000/plants')
             <img class="plant_img", src="./imgs/${el.image}">
             <div class="plant_textCon">
             <div class="plant_title">${el.title}<div>
-            <div class="plant_price">$${el.price}<div>
-            <div class="plant_pating">${el.rating}<div><div>
+            <div class="plant_pating">${el.rating}<div>
+            <div class="plant_price">$${el.price}.00<div>
+            <div>
             <div>
 
 
