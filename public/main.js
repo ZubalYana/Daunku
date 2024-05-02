1d5bc927e4524032e2662e9b5886fc1f6d63f213
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-02 22:17:27 +0300

    add the plant img background and text container

--- public/main.js
+++ public/main.js
@@ -4,10 +4,15 @@ axios.get('http://localhost:3000/plants')
     for(let el of res.data){
         $('.plantsContainer').append(
             `<div class="plant">
+            <div class="plant_imgCon">
             <img class="plant_img", src="./imgs/${el.image}">
+            <div class="plant_textCon">
             <div class="plant_title">${el.title}<div>
             <div class="plant_price">$${el.price}<div>
-            <div class="plant_pating">${el.rating}<div>
+            <div class="plant_pating">${el.rating}<div><div>
+            <div>
+
+
             <div>`
         )
     }
