8e21e9c0a06bec7fcf80572189e2f9c1b1be1f04
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-07 15:50:43 +0300

    display the existing plants from the database in the admin panel

--- public/admin/main.js
+++ public/admin/main.js
@@ -1,35 +1,3 @@
-//theme changing
-let theme = localStorage.getItem('theme') || 'light';
-$('.theme').click(function(){
-    if(theme == 'light'){
-        theme = 'dark';
-        localStorage.setItem('theme', theme);
-        changeTheme(theme);
-
-    }else{
-        theme = 'light';
-        localStorage.setItem('theme', theme);
-        changeTheme(theme);
-    }
-})
-function changeTheme(theme){
-    if(theme == 'light'){
-        $('.theme').css('justify-content', 'flex-start')
-        $('.theme').css('background-color', '#566270')
-        $('.theme_circle').css('background-color', '#fff')
-        $('.theme_circle').css('border', '2px #566270 solid')
-
-    }else{
-        $('.theme').css('justify-content', 'flex-end')
-        $('.theme').css('background-color', '#fff')
-        $('.theme_circle').css('background-color', '#566270')
-        $('.theme_circle').css('border', '2px #fff solid')
-
-
-    }
-}
-changeTheme(theme);
-
 //pages changing
 $('.content').append(
     `
@@ -86,3 +54,54 @@ $('#orders').click(()=>{
         `
     )
 })
+
+//existing plants displaying
+axios.get('http://localhost:3000/plants')
+.then((res)=>{
+    console.log(res.data)
+    for(let el of res.data){
+        $('.plantslist').append(
+            `
+            <div class="plant">
+            <img class="plantImg" src="./imgs/${el.image}" alt="">
+            <h3 class="plantName">${el.title}</h3>
+            <div class="plantRating">${el.rating}</div>
+            <div class="plantPrice">${el.price}</div>
+        </div>
+            `
+        )
+    }
+})
+
+//theme changing
+let theme = localStorage.getItem('theme') || 'light';
+$('.theme').click(function(){
+    if(theme == 'light'){
+        theme = 'dark';
+        localStorage.setItem('theme', theme);
+        changeTheme(theme);
+
+    }else{
+        theme = 'light';
+        localStorage.setItem('theme', theme);
+        changeTheme(theme);
+    }
+})
+function changeTheme(theme){
+    if(theme == 'light'){
+        $('.theme').css('justify-content', 'flex-start')
+        $('.theme').css('background-color', '#566270')
+        $('.theme_circle').css('background-color', '#fff')
+        $('.theme_circle').css('border', '2px #566270 solid')
+
+    }else{
+        $('.theme').css('justify-content', 'flex-end')
+        $('.theme').css('background-color', '#fff')
+        $('.theme_circle').css('background-color', '#566270')
+        $('.theme_circle').css('border', '2px #fff solid')
+
+
+    }
+}
+changeTheme(theme);
+
