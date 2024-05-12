1d1884b6968719079468d911309185d0cdc45d8c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-12 22:54:27 +0300

    display the amount of plant chosen instead of dublicating them multiply times, fix the plant deleting btn apperience

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
 //plants functionality
 let db = [];
+let cartlist = [];
+let totalAmount = 0;
+
 axios.get('http://localhost:3000/plants')
 .then((res)=>{
     console.log(res.data)
@@ -8,151 +11,104 @@ axios.get('http://localhost:3000/plants')
         $('.plantsContainer').append(
             `
             <div class="plant">
-            <div class="plant_imgCon">
-                <img class="plant_img" src="./imgs/${el.image}">
-                <div class="plant_textCon">
-                    <div class="plant_title">${el.title}</div>
-                    <div class="plant_rating"></div>
-                    <div class="plant_PriceAddCon">
-                        <div class="plant_price">$${el.price}.00</div>
-                        <div class="plant_addBtn" id="${el._id}">+</div>
+                <div class="plant_imgCon">
+                    <img class="plant_img" src="./imgs/${el.image}">
+                    <div class="plant_textCon">
+                        <div class="plant_title">${el.title}</div>
+                        <div class="plant_rating"></div>
+                        <div class="plant_PriceAddCon">
+                            <div class="plant_price">$${el.price}.00</div>
+                            <div class="plant_addBtn" id="${el._id}">+</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
             `
         )
-        if(el.rating == 5){
-            $('.plant_rating').empty();
-            $('.plant_rating').append(
-                `<i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>`
-            )
-        }else if(el.rating == 4){
-            $('.plant_rating').empty();
-            $('.plant_rating').append(
-                `<i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-regular fa-star"></i>`
-            )
-        }else if(el.rating == 3){
-            $('.plant_rating').empty();
-            $('.plant_rating').append(
-                `<i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>`
-            )
-        }else if(el.rating == 2){
-            $('.plant_rating').empty();
-            $('.plant_rating').append(
-                `<i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>`
-            )
-        }else if(el.rating == 1){
-            $('.plant_rating').empty();
-            $('.plant_rating').append(
-                `<i class="fa-solid fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>`
-            )
+        $('.plant_rating').empty();
+        for (let i = 0; i < 5; i++) {
+            if (i < el.rating) {
+                $('.plant_rating').append(`<i class="fa-solid fa-star"></i>`);
+            } else {
+                $('.plant_rating').append(`<i class="fa-regular fa-star"></i>`);
+            }
         }
     }
     console.log(db)
-    let cartlist = [];
-    let totalAmount = 0;
 
     $('.plant_addBtn').click((e)=>{
         $('.addingMessage').css('display', 'flex')
         setTimeout(() => {
             $('.addingMessage').css('display', 'none')
         }, 2000);
-        for(let el of db){
-            if(el._id == e.target.id){
-                cartlist.push(el)
-                console.log(cartlist)
-            }
+
+        let plantID = e.target.id;
+        let existingPlant = cartlist.find(plant => plant._id === plantID);
+
+        if (existingPlant) {
+            existingPlant.amount++;
+        } else {
+            let plantToAdd = db.find(plant => plant._id === plantID);
+            plantToAdd.amount = 1;
+            cartlist.push(plantToAdd);
         }
-        $('.mainPage_cartCounter').html(cartlist.length)
-        $('.chosenPlantsContainer').empty();
-        
-        for(let el of cartlist){
-            $('.chosenPlantsContainer').append(
-                `<div class="chosenPlant">
-                <img class="chosenPlant_img" src="./imgs/${el.image}" alt="">
+
+        renderCart();
+    });
+});
+
+function renderCart() {
+    $('.mainPage_cartCounter').html(cartlist.length);
+    $('.chosenPlantsContainer').empty();
+    totalAmount = 0;
+
+    cartlist.forEach(plant => {
+        $('.chosenPlantsContainer').append(
+            `<div class="chosenPlant">
+                <img class="chosenPlant_img" src="./imgs/${plant.image}" alt="">
                 <div class="chosenPlant_namePriceCon">
-                    <div class="chosenPlant_name">${el.title}</div>
-                    <div class="chosenPlant_price">$${el.price}.00</div>
+                    <div class="chosenPlant_name">${plant.title}</div>
+                    <div class="chosenPlant_price">$${plant.price}.00</div>
                 </div>
-                <div class="chosenPlant_amount">x2</div>
-                <div class="chosenPlant_delete" id="deleteFromCart${el._id}"><div class="minus"></div></div>
+                <div class="chosenPlant_amount">x${plant.amount}</div>
+                <div class="chosenPlant_delete" id="deleteFromCart${plant._id}"><div class="minus"></div></div>
             </div>`
-            )
-            totalAmount += el.price; 
-            console.log(totalAmount)
-        }
-        
-        $('#totalAmount').html(`Total amount: $${totalAmount}.00`); 
-        $('#chosenPlantCount').html(`Plants chosen: ${cartlist.length}`)
-    })
-    
-    $('.mainPage_cart').click(()=>{
-        $('.cartPopupContainer').css('display', 'flex')
-    })
-    $('#cartPopup_xmark').click(()=>{
-        $('.cartPopupContainer').css('display', 'none')
-    })
-    //hover effects 
-    $('.chosenPlantsContainer').on('mouseenter', '.chosenPlant_delete', function() {
+        );
+        totalAmount += plant.price * plant.amount;
+    });
+
+    $('#totalAmount').html(`Total amount: $${totalAmount}.00`);
+    $('#chosenPlantCount').html(`Plants chosen: ${cartlist.length}`);
+}
+
+$('.mainPage_cart').click(() => {
+    $('.cartPopupContainer').css('display', 'flex');
+});
+
+$('#cartPopup_xmark').click(() => {
+    $('.cartPopupContainer').css('display', 'none');
+});
+
+// Hover effects 
+$('.chosenPlantsContainer').on('mouseenter', '.chosenPlant_delete', function() {
     $(this).css('background-color', '#081323');
     $(this).find('.minus').css('background-color', '#fff');
-    }).on('mouseleave', '.chosenPlant_delete', function() {
+}).on('mouseleave', '.chosenPlant_delete', function() {
     $(this).css('background-color', '#fff');
     $(this).find('.minus').css('background-color', '#081323');
+});
 
-    }); 
-
-    //plant deleting from the cartList
-    $('.chosenPlantsContainer').on('click', '.chosenPlant_delete', function(e) {
+// Plant deleting from the cartList
+$('.chosenPlantsContainer').on('click', '.chosenPlant_delete', function(e) {
     e.stopPropagation();
     let ID = e.target.id;
     if (ID.substring(0, 14) === 'deleteFromCart') {
         let plantID = ID.substring(14);
-        cartlist = cartlist.filter(el => el._id !== plantID);
-        $('.mainPage_cartCounter').html(cartlist.length);
-        renderChosenPlants();
+        cartlist = cartlist.filter(plant => plant._id !== plantID);
+        renderCart();
     }
-    });
-    function renderChosenPlants() {
-    $('.chosenPlantsContainer').empty();
-    for (let el of cartlist) {
-        $('.chosenPlantsContainer').append(
-            `<div class="chosenPlant">
-                <img class="chosenPlant_img" src="./imgs/${el.image}" alt="">
-                <div class="chosenPlant_namePriceCon">
-                    <div class="chosenPlant_name">${el.title}</div>
-                    <div class="chosenPlant_price">$${el.price}.00</div>
-                </div>
-                <div class="chosenPlant_delete" id="deleteFromCart${el._id}"><div class="minus"></div></div>
-            </div>`
-        );
-    }
-    }
+});
 
-
-
-})
 
 //cookies animation
 $('.cookiesJar_con').hover(
